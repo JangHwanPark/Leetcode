a13e9cf6a63218656eabd21843b996493d5151bc
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-03-17 13:06:35 +0900

    Time: 73 ms (32.17%), Space: 67.1 MB (17.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,35 +3,38 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let count = 0;
-    const dx = [-1, 1, 0, 0];
-    const dy = [0, 0, -1, 1];
-    const n = grid.length;
-    const m = grid[0].length;
+    const dx=[-1,1,0,0];
+    const dy=[0,0,-1,1];
+    const r=grid.length;
+    const c=grid[0].length;
+    const v=Array.from({length:r},()=>Array(c).fill(false));
+    let cnt=0;
 
-    for (let r = 0; r < n; r++) {
-        for (let c = 0; c < m; c++) {
-            const queue = [[r, c]];
-            
-            if (grid[r][c] === '1') {
-                count++;
-                
-                while (queue.length > 0) {
-                    const [x, y] = queue.shift();
-                
-                    for (let i = 0; i < 4; i++) {
-                        const nx = x + dx[i];
-                        const ny = y + dy[i];
-
-                        if (nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] === '1') {
-                            grid[nx][ny] = 0;
-                            queue.push([nx, ny]);
-                        }
-                    }
-                }
+    function bfs(sx,sy){
+        const q=[[sx,sy]];
+        v[sx][sy]=true;
+        while(q.length>0){
+            const [x,y]=q.shift();
+            for(let i=0;i<4;i++){
+                const nx=x+dx[i];
+                const ny=y+dy[i];
+                if(nx<0||ny<0||nx>=r||ny>=c)continue;
+                if(grid[nx][ny]!=="1")continue;
+                if(v[nx][ny])continue;
+                v[nx][ny]=true;
+                q.push([nx,ny])
             }
         }
     }
 
-    return count;
+    for(let i=0;i<r;i++){
+        for(let j=0;j<c;j++){
+            if(grid[i][j]==="1"&&!v[i][j]) {
+                cnt++;
+                bfs(i,j);
+            }
+        }
+    }
+
+    return cnt;
 };
