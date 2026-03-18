cbc6df70c1823b5621fb1c39c4323a86c4feb97a
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-03-18 15:42:12 +0900

    Time: 52 ms (91.47%), Space: 58.3 MB (84.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,38 +3,32 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    const dx=[-1,1,0,0];
-    const dy=[0,0,-1,1];
+    let cnt=0;
     const r=grid.length;
     const c=grid[0].length;
     const v=Array.from({length:r},()=>Array(c).fill(false));
-    let cnt=0;
+    const dx=[-1,1,0,0];
+    const dy=[0,0,-1,1];
 
-    function bfs(sx,sy){
-        const q=[[sx,sy]];
-        v[sx][sy]=true;
-        while(q.length>0){
-            const [x,y]=q.shift();
-            for(let i=0;i<4;i++){
-                const nx=x+dx[i];
-                const ny=y+dy[i];
-                if(nx<0||ny<0||nx>=r||ny>=c)continue;
-                if(grid[nx][ny]!=="1")continue;
-                if(v[nx][ny])continue;
-                v[nx][ny]=true;
-                q.push([nx,ny])
-            }
+    function dfs(x,y){
+        if(x<0||y<0||x>=r||y>=c)return;
+        if(grid[x][y]!=="1")return;
+        if(v[x][y])return;
+        v[x][y]=true;
+        for(let i=0;i<4;i++){
+            const nx=x+dx[i];
+            const ny=y+dy[i];
+            dfs(nx,ny);
         }
     }
 
     for(let i=0;i<r;i++){
         for(let j=0;j<c;j++){
-            if(grid[i][j]==="1"&&!v[i][j]) {
+            if(grid[i][j]==="1"&&!v[i][j]){
                 cnt++;
-                bfs(i,j);
+                dfs(i,j);
             }
         }
     }
-
     return cnt;
 };
