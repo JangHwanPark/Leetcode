b10e392f31d8a9e24e1b8aef94b2a65511c08ae0
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-03-24 11:36:08 +0900

    Time: 2 ms (37.58%), Space: 55.2 MB (58.4%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -6,17 +6,16 @@
 var addBinary = function(a, b) {
     let i = a.length - 1;
     let j = b.length - 1;
-    let carry = 0;
-    let res = '';
+    let c = 0;
+    let r = '';
 
-    while (i >= 0 || j >= 0 || carry > 0){
-    const x = i >= 0 ? Number(a[i--]) : 0;
-    const y = j >= 0 ? Number(b[j--]) : 0;
-    const sum = x + y + carry;
-    carry = Math.floor(sum / 2);
-    res = (sum % 2) + res;
-    console.log(sum, res)
+    while(i>=0||j>=0||c>0){
+        const x = i >= 0 ? Number(a[i--]) : 0;
+        const y = j >= 0 ? Number(b[j--]) : 0;
+        const sum = x + y + c;
+        c = Math.floor(sum / 2);
+        r = (sum % 2) + r;
     }
 
-    return res;
+    return r;
 };
