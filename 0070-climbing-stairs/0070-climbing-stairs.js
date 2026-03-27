a8d375d8f379df390d47746acda828852f52c005
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-03-27 12:29:54 +0900

    Time: 3 ms (2.25%), Space: 54.3 MB (13.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,22 +2,14 @@
  * @param {number} n
  * @return {number}
  */
-
-
-const climbStairs = function(n) {
-    // 메모이제이션
-    const memo = Array(n + 1).fill(-1);
-    const result = dp(memo, n);
-    return result;
+var climbStairs = function(n) {
+    const dp = Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (let i = 3; i < n + 1; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    console.log(dp, n, n+1)
+    return dp[n];
 };
-
-// 재귀함수
-function dp(memo, n) {
-    // 기저조건
-    if (n <= 1) return 1;
-    // 이미 계산했으면 반환
-    if (memo[n] !== -1) return memo[n];
-    // n - 1 + n - 2
-    memo[n] = dp(memo, n - 1) + dp(memo, n - 2);
-    return memo[n];
-}
