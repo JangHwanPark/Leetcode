926753d8c68498c41d6b8bafe9e38ce79a725848
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-03-31 14:36:44 +0900

    Time: 2 ms (5.02%), Space: 54.2 MB (16.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 var rob = function(nums) {
-    console.log(nums)
-    const dp = Array(nums.length+1).fill(0);
-    dp[1] = nums[0];
-    dp[2] = Math.max(nums[0],nums[1]);
+    const n = nums.length;
+    const dp = Array(n).fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
 
-    for (let i = 2; i < nums.length + 1; i++) {
-        dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i-1]);
+    for (let i = 2; i < n; i++) {
+        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
     }
-
-    console.log(dp)
-    return dp[nums.length]
-};
+    console.log("dp[n]:",dp[n - 1])
+    return dp[n - 1]
+}; 
