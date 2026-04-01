b98f8766de84921f6f320511a9ea459437c7e0f3
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-04-01 12:18:36 +0900

    Time: 1 ms (47.21%), Space: 58.5 MB (38.97%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -13,26 +13,12 @@
 var maxDepth = function(root) {
     if (!root) return 0;
 
-    const queue = [[root, 1]];
-    let max_depth = 0;
-
-    while (queue.length) {
-        const [node, depth] = queue.shift();
-        // console.log("depth" + depth);
-        // console.log("node: " + JSON.stringify(node, null, 2));
-        // console.log("node.val: " + node.val);
-        max_depth = Math.max(max_depth, depth);
-
-        if (node.left) {
-            queue.push([node.left, depth + 1]);
-        }
-        if (node.right) {
-            queue.push([node.right, depth + 1]);
-        }
+    function dfs(node) {
+        if (!node) return 0;
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        return Math.max(left, right) + 1;
     }
 
-    // console.log(max_depth);
-    return max_depth;
+    return dfs(root);
 };
-
-console.log(maxDepth);
