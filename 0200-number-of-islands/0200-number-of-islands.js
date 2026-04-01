bf9b40d082771477339062481f1fcd55576db804
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-04-01 16:49:56 +0900

    Time: 65 ms (50.99%), Space: 59.7 MB (48.52%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,32 +3,35 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let cnt=0;
-    const r=grid.length;
-    const c=grid[0].length;
-    const v=Array.from({length:r},()=>Array(c).fill(false));
-    const dx=[-1,1,0,0];
-    const dy=[0,0,-1,1];
+    const row = grid.length;
+    const col = grid[0].length;
+    const v = Array.from({length: row}, () => Array(col).fill(-1));
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
+    let cnt = 0;
 
-    function dfs(x,y){
-        if(x<0||y<0||x>=r||y>=c)return;
-        if(grid[x][y]!=="1")return;
-        if(v[x][y])return;
-        v[x][y]=true;
-        for(let i=0;i<4;i++){
-            const nx=x+dx[i];
-            const ny=y+dy[i];
-            dfs(nx,ny);
+    function dfs(r, c) {
+        v[r][c] = 1;
+        for (let i = 0; i < 4; i++) {
+            const nx = r + dx[i];
+            const ny = c + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= row || ny >= col) continue;
+            if (grid[nx][ny] !== "1") continue;
+            if (v[nx][ny] !== -1) continue;
+            dfs(nx, ny);
         }
     }
 
-    for(let i=0;i<r;i++){
-        for(let j=0;j<c;j++){
-            if(grid[i][j]==="1"&&!v[i][j]){
+    // 초기값
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (grid[i][j] === "1" && v[i][j] === -1) {
                 cnt++;
-                dfs(i,j);
+                dfs(i, j)
             }
         }
     }
+
     return cnt;
 };
