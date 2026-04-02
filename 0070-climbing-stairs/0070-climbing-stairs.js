5f0bfd327304ee191f41c5702f9abe15b90d6fe1
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-04-02 11:23:20 +0900

    Time: 5 ms (1.6%), Space: 54.2 MB (13.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -4,19 +4,13 @@
  */
 var climbStairs = function(n) {
     const dp = Array(n + 1).fill(0);
-    
-    // n이 작은 경우 예외처리
-    if (n === 1) return 1;
-    if (n === 2) return 2;
-  
-    // dp 초기화
-    dp[0] = 0;
     dp[1] = 1;
     dp[2] = 2;
 
     for (let i = 3; i < n + 1; i++) {
         dp[i] = dp[i - 1] + dp[i - 2];
     }
-    
+    console.log(dp)
+    console.log(dp[n])
     return dp[n]
 };
