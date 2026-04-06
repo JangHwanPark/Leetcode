dfbc59304cd5d9a7245b2541b477953ae97de815
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-04-06 11:22:48 +0900

    Time: 29 ms (5.11%), Space: 63.9 MB (5.36%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -6,25 +6,34 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, color) {
-    console.log(image)
-    const dx=[-1,1,0,0];
-    const dy=[0,0,-1,1];
-    const r=image.length;
-    const c=image[0].length;
-    const q=[[sr,sc]]
-    const org=image[sr][sc];
-    if(org===color)return image;
-    image[sr][sc]=color;
-    while(q.length>0){
-        const [x,y]=q.shift();
-        for(let i=0;i<4;i++){
-            const nx=x+dx[i];
-            const ny=y+dy[i];
-            if(nx<0||ny<0||nx>=r||ny>=c)continue;
-            if(image[nx][ny]!==org)continue;
-            image[nx][ny]=color;
-            q.push([nx,ny]);
+    const m = image.length;
+    const n = image[0].length;
+    const visited = Array.from({length: image.length}, () => Array(image[0].length).fill(false));
+    const st = image[sr][sc];
+    const queue = [[sr,sc]];
+    const dx = [-1,1,0,0];
+    const dy = [0,0,-1,1];
+    visited[sr][sc] = true;
+    image[sr][sc] = color;
+    
+
+    while (queue.length > 0) {
+        const [x,y] = queue.shift();
+
+        // 인접한 픽섹 확인 = 4방향 탐색
+        for (let i = 0; i < 4; i++) {
+            const nx = x + dx[i];
+            const ny = y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+            if (visited[nx][ny]) continue;
+            if (image[nx][ny] !== st) continue;
+            visited[nx][ny] = true;
+            image[nx][ny] = color;
+            queue.push([nx, ny])
         }
     }
+    console.log(visited)
+    // 연산한 이미지 반환
     return image;
 };
