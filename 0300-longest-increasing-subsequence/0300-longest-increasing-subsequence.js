377a27379738b03b2ed19b2c9f94c1b624a9ffb7
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-04-07 12:29:26 +0900

    Time: 62 ms (62.03%), Space: 57.6 MB (21.76%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    const dp = Array(nums.length).fill(1);
+
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+    console.log(dp)
+    return Math.max(...dp);
+};
