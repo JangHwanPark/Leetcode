638eb427413850d36f8049e0ca3270657933bed6
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-04-13 16:08:03 +0900

    Time: 6 ms (65.44%), Space: 55.8 MB (68.89%) - LeetHub

---
+++ 0139-word-break/0139-word-break.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    const word = new Set(wordDict);
+    const dp = Array(s.length + 1).fill(false);
+    dp[0] = true;
+
+    for (let i = 1; i <= s.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (dp[j] && word.has(s.slice(j, i))) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    return dp[s.length];
+};
